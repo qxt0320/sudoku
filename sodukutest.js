730b37918bfa4b335f5d26740a07df38acbe8f5b
qxt0320 <894790421@qq.com>
2023-09-24 13:24:48 +0800

    单元测试

---
+++ sodukutest.js
@@ -0,0 +1,41 @@
+const sudoku = require('./soduku.js'); // replace this with the actual module path
+describe('Sudoku tests', () => {
+  test('getCell', () => {
+  // setup
+  const mockSudoku = [
+    [5, 3, 0, 0, 7, 0, 0, 0, 0],
+    [6, 0, 0, 1, 9, 5, 0, 0, 0],
+    [0, 9, 8, 0, 0, 0, 0, 6, 0],
+    [8, 0, 0, 0, 6, 0, 0, 0, 3],
+    [4, 0, 0, 8, 0, 3, 0, 0, 1],
+    [7, 0, 0, 0, 2, 0, 0, 0, 6],
+    [0, 6, 0, 0, 0, 0, 2, 8, 0],
+    [0, 0, 0, 4, 1, 9, 0, 0, 5],
+    [0, 0, 0, 0, 8, 0, 0, 7, 9]
+  ];
+  // action
+  const result = sudoku.getCell(mockSudoku, 0, 1); // replace x and y with coordinates
+  // assertion
+  expect(result).toBe(3);
+  });
+
+  test('setCell', () => {
+  // setup
+  const mockSudoku = [
+    [5, 3, 0, 0, 7, 0, 0, 0, 0],
+    [6, 0, 0, 1, 9, 5, 0, 0, 0],
+    [0, 9, 8, 0, 0, 0, 0, 6, 0],
+    [8, 0, 0, 0, 6, 0, 0, 0, 3],
+    [4, 0, 0, 8, 0, 3, 0, 0, 1],
+    [7, 0, 0, 0, 2, 0, 0, 0, 6],
+    [0, 6, 0, 0, 0, 0, 2, 8, 0],
+    [0, 0, 0, 4, 1, 9, 0, 0, 5],
+    [0, 0, 0, 0, 8, 0, 0, 7, 9]
+  ];
+  // action
+  sudoku.setCell(mockSudoku, 0, 2, 4); // replace x, y, and value with what you want to insert
+  // assertion
+  expect(sudoku.getCell(mockSudoku, 0, 2)).toBe(4);
+  });
+
+});
